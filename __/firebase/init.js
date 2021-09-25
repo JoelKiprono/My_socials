9a8181c480f3bcee14e4dba65acf8d1b64d2463f
Joel3449 <joelkip3449@gmail.com>
2021-09-25 03:32:33 +0300

    initial commit

---
+++ __/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyDIKZiZ4-K3nKVxJN3KRukLs8pjz8oWyjo",
+  "appId": "1:1061327926507:web:524008d656759af3dc7aae",
+  "authDomain": "kanaicontact.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-1B8Z19BF3V",
+  "messagingSenderId": "1061327926507",
+  "projectId": "kanaicontact",
+  "storageBucket": "kanaicontact.appspot.com"
+});
